f56318d115009e562dd9ec5944d4a361e7ce1d27
sachith94-lion <sachithmanuradha@gmail.com>
2021-04-16 17:13:05 +0530

    auth function done

---
+++ Cafe-Manager/Pods/Target Support Files/Pods-Cafe-Manager-Cafe-ManagerUITests/Pods-Cafe-Manager-Cafe-ManagerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cafe_Manager_Cafe_ManagerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Cafe_Manager_Cafe_ManagerUITests
+@end
---
+++ Cafe-Manager/Pods/Target Support Files/Pods-Cafe-ManagerTests/Pods-Cafe-ManagerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cafe_ManagerTests : NSObject
+@end
+@implementation PodsDummy_Pods_Cafe_ManagerTests
+@end
