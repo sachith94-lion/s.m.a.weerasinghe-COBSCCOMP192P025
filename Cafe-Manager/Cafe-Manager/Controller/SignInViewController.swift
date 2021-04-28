395e8720c4bd6c4fda91793485584a21eaad2b8c
sachith94-lion <sachithmanuradha@gmail.com>
2021-04-28 11:16:12 +0530

    validation and home view done

--- Cafe-Manager/Cafe-Manager/Controller/SignInViewController.swift
+++ Cafe-Manager/Cafe-Manager/Controller/SignInViewController.swift
@@ -26,37 +26,23 @@ class SignInViewController: UIViewController {
     }
     
     @IBAction func onSignInPressed(_ sender: UIButton) {
-        if validateInput(){
-            authenticateUser(email: txtEmail.text!, password: txtPassword.text!)
-            
-        } else {
-            print("INput error found")
+        
+        if !InputValidator.isValidEmail(email: txtEmail.text ?? ""){
+            Loaf("Invalid email address!", state: .error, sender: self).show()
+            return
         }
+        
+        if !InputValidator.isValidPassword(pass: txtPassword.text ?? "", minLength: 6, maxLength: 50){
+            Loaf("Invalid Password", state: .error, sender: self).show()
+            return
+        }
+        authenticateUser(email: txtEmail.text!, password: txtPassword.text!)
+        
+        
+        
     }
     
-    func validateInput() -> Bool {
-        
-        guard let email = txtEmail.text else {
-            print("Email is NULL")
-            return false
-        }
-        
-        guard let password = txtPassword.text else{
-            print("Password is NULL")
-            return false
-        }
-        
-        if email.count < 5 {
-            print("Enter a valid Email")
-            return false
-        }
-        
-        if password.count < 5 {
-            print("Enter a valid Password")
-            return false
-        }
-        return true
-    }
+    
     
     func authenticateUser(email: String, password: String) {
         Auth.auth().signIn(withEmail: email, password: password) {
--- Cafe-Manager/Cafe-Manager/Controller/SignUpViewController.swift
+++ Cafe-Manager/Cafe-Manager/Controller/SignUpViewController.swift
@@ -16,93 +16,87 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var txtPhone: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     
+    var ref: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func onSignUpPressed(_ sender: UIButton) {
-        if validateInput() {
-            registerUser(email: txtEmail.text!, password: txtPassword.text!)
+        
+        if !InputValidator.isValidName(name: txtname.text ?? ""){
+            Loaf("Invalid Name", state: .error, sender: self).show()
+            return
         }
-        else{
-            print("Input error found")
+        
+        if !InputValidator.isValidEmail(email: txtEmail.text ?? ""){
+            Loaf("Invalid email address!", state: .error, sender: self).show()
+            return
         }
+        
+        if !InputValidator.isValidMobileNo(txtPhone.text ?? ""){
+            Loaf("Invalid phone number", state: .error, sender: self).show()
+            return
+        }
+        
+        if !InputValidator.isValidPassword(pass: txtPassword.text ?? "", minLength: 6, maxLength: 50){
+            Loaf("Invalid Password", state: .error, sender: self).show()
+            return
+        }
+        
+        let user = User(userName: txtname.text ?? "", userEmail: txtEmail.text ?? "",  userPassword: txtPassword.text ?? "", userPhone: txtPhone.text ?? "")
+        
+        registerUser(user: user)
+        
+        
     }
     
     @IBAction func onSignInPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
-    func registerUser(email: String, password: String) {
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+    func registerUser(user: User) {
+        Auth.auth().createUser(withEmail: user.userEmail , password: user.userPassword) { authResult, error in
             if let err = error {
                 print(err.localizedDescription)
                 Loaf("User sign up failed!", state: .error, sender: self).show()
                 return
             }
             
-            if let result = authResult{
-                print("User added with Email: \(result.user.email ?? "Not Found")")
-                Loaf("User registered successfully!", state: .success, sender: self).show()
-            }
+            
+            
+            
+            self.saveUserData(user: user)
             
         }
     }
     
-    func validateInput() -> Bool{
+    func saveUserData(user: User) {
         
-        guard let name = txtname.text else{
-            print("Name is NULL")
-            return false
+        let userData = [
+            "userName" : user.userName,
+            "userEmail" : user.userEmail,
+            "userPhone" : user.userPhone,
+            "userPassword" : user.userPassword
+        ]
+        
+        self.ref.child("users")
+            .child(user.userEmail.replacingOccurrences(of: "@", with: "_")
+                    .replacingOccurrences(of: ".", with: "_")).setValue(userData) {
+            (error, ref) in
+            
+            if let err = error {
+                print(err.localizedDescription)
+                Loaf("User data not saved on database", state: .error, sender: self).show()
+                return
+            }
+                        Loaf("User data saved on database", state: .success, sender: self).show {
+                            type in
+                            self.dismiss(animated: true, completion: nil)
+                        }
         }
-        
-        guard let email = txtEmail.text else{
-            print("Email is NULL")
-            return false
-        }
-        
-        guard let phone = txtPhone.text else{
-            print("Phone is NULL")
-            return false
-        }
-        
-        guard let password = txtPassword.text else{
-            print("Password is NULL")
-            return false
-        }
-        
-        if name.count < 5 {
-            print("Enter a valid name")
-            return false
-        }
-        
-        if email.count < 5 {
-            print("Enter a valid email")
-            return false
-        }
-        
-        if phone.count < 10 {
-            print("Enter a valid phone")
-            return false
-        }
-        
-        if password.count < 5 {
-            print("Enter a valid password")
-            return false
-        }
-        
-        return true
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
